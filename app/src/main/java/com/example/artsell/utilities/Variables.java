033f66b587b3eb216e668c0bb2b02d206071bcc1
TahsinTunan <53114840+TahsinTunan@users.noreply.github.com>
2021-07-20 07:20:25 +0600

    URL updated in utilities/variables.java

--- app/src/main/java/com/example/artsell/utilities/Variables.java
+++ app/src/main/java/com/example/artsell/utilities/Variables.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Variables {
-    public static String url="http://192.168.0.103:3000/";
+    public static String url="http://192.168.0.105:3000/";
     public static Retrofit initializeRetrofit(){
         return new Retrofit.Builder()
                 .baseUrl(url)
