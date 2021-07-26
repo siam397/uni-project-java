0350c427c40adab8708887165248983035cb566d
TahsinTunan <53114840+TahsinTunan@users.noreply.github.com>
2021-07-26 22:38:49 +0600

    ip

--- app/src/main/java/com/example/artsell/utilities/Variables.java
+++ app/src/main/java/com/example/artsell/utilities/Variables.java
@@ -5,8 +5,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Variables {
 
-    public static String url="http://192.168.0.102:3000/";
-    public static String url_2="http://192.168.0.102:4000/";
+    public static String url="http://192.168.0.103:3000/";
+    public static String url_2="http://192.168.0.103:4000/";
     public static Retrofit initializeRetrofit(){
         return new Retrofit.Builder()
                 .baseUrl(url)
